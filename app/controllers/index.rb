f71eb97bcc4ba43453fc32fcc11e43efe2c52014
cokeefe <chaemobile@gmail.com>
2013-09-10 16:07:15 -0700

    controller logic

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -3,7 +3,6 @@ get '/' do
 end
 
 get '/sign_in' do
-  # the `request_token` method is defined in `app/helpers/oauth.rb`
   redirect request_token.authorize_url
 end
 
@@ -14,14 +13,12 @@ end
 
 require 'pry'
 get '/auth' do
-  # the `request_token` method is defined in `app/helpers/oauth.rb`
   @access_token = request_token.get_access_token(:oauth_verifier => params[:oauth_verifier])
-  # our request token is only valid until we use it to get an access token, so let's delete it from our session
   session.delete(:request_token)
 
-  # at this point in the code is where you'll need to create your user account and store the access token
+
   @user = User.find_by_username(@access_token.params[:screen_name])
-  User.log_new(@access_token) unless @user.nil?
+  @user = User.log_new(@access_token) if @user.nil?
 
   erb :index
 end
