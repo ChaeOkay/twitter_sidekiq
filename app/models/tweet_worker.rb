6a4aafd6831e8b3f0a83a458fdfc510e9a8e33a8
ChaeOkay <chaemobile@gmail.com>
2013-10-23 15:06:30 -0400

    remove comments

--- app/models/tweet_worker.rb
+++ app/models/tweet_worker.rb
@@ -1,14 +1,9 @@
 class TweetWorker
   include Sidekiq::Worker
 
-# require 'pry'
   def perform(tweet_id)
-    # binding.pry
-    puts "*" * 80
-    puts "starting #{jid}"
-    puts "sleeping...."
 
-        sleep rand(10)
+    sleep(5)
     tweet = Tweet.find(tweet_id)
     user = tweet.user
 
@@ -16,12 +11,8 @@ class TweetWorker
       config.oauth_token = user.oauth_token
       config.oauth_token_secret = user.oauth_secret
     end
+    
     tweeter.update(tweet.status)
-
-    puts "...done sleeping"
-    puts "*" * 80
-    #tweet message
-    #load erb
   end
 
 end
